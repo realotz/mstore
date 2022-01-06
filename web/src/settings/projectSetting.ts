15241057b604d67323db2de997a53e125e721f56
admin@zimujun.cc <kaijian_yu@hxecloud.com>
2022-01-06 13:48:31 +0800

    web init

---
+++ web/src/settings/projectSetting.ts
@@ -0,0 +1,80 @@
+import type { ProjectConfig } from '/#/config';
+const setting: ProjectConfig = {
+  showSettingButton: true,
+  showDarkModeToggle: true,
+  settingButtonPosition: "auto",
+  permissionMode: "ROUTE_MAPPING",
+  permissionCacheType: 1,
+  sessionTimeoutProcessing: 0,
+  themeColor: "#0084f4",
+  grayMode: false,
+  colorWeak: false,
+  fullContent: false,
+  contentMode: "full",
+  showLogo: true,
+  showFooter: false,
+
+  // Header configuration
+  headerSetting: {
+    bgColor: "#394664",
+    fixed: true,
+    show: true,
+    theme: "dark",
+    useLockPage: true,
+    showFullScreen: true,
+    showDoc: true,
+    showNotice: true,
+    showSearch: false
+  },
+
+  // Menu configuration
+  menuSetting: {
+    bgColor: "#ffffff",
+    fixed: true,
+    collapsed: false,
+    collapsedShowTitle: false,
+    canDrag: false,
+    show: true,
+    hidden: false,
+    menuWidth: 210,
+    mode: "inline",
+    type: "mix",
+    theme: "light",
+    split: true,
+    topMenuAlign: "center",
+    trigger: "FOOTER",
+    accordion: true,
+    closeMixSidebarOnChange: false,
+    mixSideTrigger: "click",
+    mixSideFixed: false
+  },
+
+  // Multi-label
+  multiTabsSetting: {
+    cache: false,
+    show: true,
+    canDrag: true,
+    showQuick: true,
+    showRedo: true,
+    showFold: true
+  },
+
+  // Transition Setting
+  transitionSetting: {
+    enable: true,
+    basicTransition: "fade-slide",
+    openPageLoading: true,
+    openNProgress: false
+  },
+  openKeepAlive: true,
+  lockTime: 0,
+  showBreadCrumb: true,
+  showBreadCrumbIcon: true,
+  useErrorHandle: false,
+  useOpenBackTop: true,
+  canEmbedIFramePage: true,
+  closeMessageOnSwitch: true,
+  removeAllHttpPending: false
+};
+
+export default setting;
